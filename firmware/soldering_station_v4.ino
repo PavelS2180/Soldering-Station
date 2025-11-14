85f709de97cf586122e4320bf5c816cc11a9c718
PavelS2180 <pavel@example.com>
2025-11-14 15:29:18 +0200

    Complete project status update and bug fixes

--- firmware/soldering_station_v4.ino
+++ firmware/soldering_station_v4.ino
@@ -430,6 +430,52 @@ bool downloadAndInstallUpdate(String downloadUrl) {
   return false;
 }
 
+// -------- Base64 кодирование --------
+String base64Encode(String input) {
+  const char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  String output = "";
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
+  int len = input.length();
+  
+  for (int idx = 0; idx < len; idx++) {
+    char_array_3[i++] = input.charAt(idx);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+      
+      for (i = 0; i < 4; i++) {
+        output += base64_chars[char_array_4[i]];
+      }
+      i = 0;
+    }
+  }
+  
+  if (i > 0) {
+    for (j = i; j < 3; j++) {
+      char_array_3[j] = '\0';
+    }
+    
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    
+    for (j = 0; j < i + 1; j++) {
+      output += base64_chars[char_array_4[j]];
+    }
+    
+    while (i++ < 3) {
+      output += '=';
+    }
+  }
+  
+  return output;
+}
+
 // -------- Загрузка логов на GitHub --------
 bool uploadLogToGitHub(String logContent, String filename) {
   if (!githubLogging || WiFi.status() != WL_CONNECTED) {
@@ -441,7 +487,7 @@ bool uploadLogToGitHub(String logContent, String filename) {
   String url = GITHUB_API_URL + "/contents/logs/" + filename;
   
   // Кодируем содержимое в base64
-  String encodedContent = base64::encode(logContent);
+  String encodedContent = base64Encode(logContent);
   
   JsonDocument doc;
   doc["message"] = "Auto-uploaded log: " + filename;
